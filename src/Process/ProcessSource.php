6c269d5e988e695548a351fbf913d95daf759733
gclift <gclift@gmail.com>
2022-12-16 07:45:55 -0700

    grc - added a generic process source

---
+++ src/Process/ProcessSource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace NoLoCo\Core\Process;
+
+use Nodez\Core\Process\ProcessSourceInterface;
+
+/**
+ * A generic process source that will hold a collection
+ * of processes and return using the interface's get processes
+ * method.
+ */
+class ProcessSource implements ProcessSourceInterface
+{
+
+    public function __construct(
+        private iterable $sources = []
+    ){
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getProcesses(): array
+    {
+        return $this->sources;
+    }
+}
