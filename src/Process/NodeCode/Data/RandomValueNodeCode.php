c75b5a912cb6560e52a44c89336d4eb56ade70fb
gclift <gclift@gmail.com>
2023-01-07 09:07:56 -0700

    grc - added random number generator nade

---
+++ src/Process/NodeCode/Data/RandomValueNodeCode.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Feral\Core\Process\NodeCode\Data;
+
+use Exception;
+use Feral\Core\Process\Configuration\ConfigurationManager;
+use Feral\Core\Process\Context\ContextInterface;
+use Feral\Core\Process\Exception\MissingConfigurationValueException;
+use Feral\Core\Process\NodeCode\Category\NodeCodeCategoryInterface;
+use Feral\Core\Process\NodeCode\Configuration\Description\ConfigurationDescriptionInterface;
+use Feral\Core\Process\NodeCode\Configuration\Description\StringArrayConfigurationDescription;
+use Feral\Core\Process\NodeCode\Configuration\Description\StringConfigurationDescription;
+use Feral\Core\Process\NodeCode\NodeCodeInterface;
+use Feral\Core\Process\NodeCode\Traits\ConfigurationTrait;
+use Feral\Core\Process\NodeCode\Traits\ConfigurationValueTrait;
+use Feral\Core\Process\NodeCode\Traits\ContextMutationTrait;
+use Feral\Core\Process\NodeCode\Traits\ContextValueTrait;
+use Feral\Core\Process\NodeCode\Traits\EmptyConfigurationDescriptionTrait;
+use Feral\Core\Process\NodeCode\Traits\NodeCodeMetaTrait;
+use Feral\Core\Process\NodeCode\Traits\OkResultsTrait;
+use Feral\Core\Process\NodeCode\Traits\ResultsTrait;
+use Feral\Core\Process\Result\ResultInterface;
+use Feral\Core\Utility\Filter\Comparator\Exception\UnknownComparatorException;
+use Feral\Core\Utility\Search\DataPathReader;
+use Feral\Core\Utility\Search\DataPathReaderInterface;
+use Feral\Core\Utility\Search\DataPathWriter;
+
+/**
+ * Create a random number between 0 and 1
+ *
+ * Configuration Keys
+ *  context_path - The path in the context to set the random number
+ *
+ */
+class RandomValueNodeCode implements NodeCodeInterface
+{
+    use NodeCodeMetaTrait,
+        ResultsTrait,
+        ConfigurationTrait,
+        ConfigurationValueTrait,
+        EmptyConfigurationDescriptionTrait,
+        ContextValueTrait,
+        ContextMutationTrait,
+        OkResultsTrait;
+
+    const KEY = 'random';
+    const NAME = 'Random';
+    const CONTEXT_PATH = 'context_path';
+    const DESCRIPTION = 'Create a random number between a min and max';
+
+
+    public function __construct(
+        DataPathReaderInterface $dataPathReader = new DataPathReader(),
+        DataPathWriter $dataPathWriter = new DataPathWriter(),
+        ConfigurationManager $configurationManager = new ConfigurationManager()
+    ) {
+        $this->setMeta(
+            self::KEY,
+            self::NAME,
+            self::DESCRIPTION,
+            NodeCodeCategoryInterface::DATA
+        )
+            ->setConfigurationManager($configurationManager)
+            ->setDataPathWriter($dataPathWriter)
+            ->setDataPathReader($dataPathReader);
+    }
+
+
+    /**
+     * @return ConfigurationDescriptionInterface[]
+     */
+    public function getConfigurationDescriptions(): array
+    {
+        return [
+            (new StringArrayConfigurationDescription())
+                ->setKey(self::CONTEXT_PATH)
+                ->setName('Context Path')
+                ->setDescription('The context path tto set the random value.'),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     * @throws     MissingConfigurationValueException|UnknownComparatorException
+     * @throws     Exception
+     */
+    public function process(ContextInterface $context): ResultInterface
+    {
+        $path = $this->getRequiredConfigurationValue(self::CONTEXT_PATH);
+        $value = mt_rand(0, mt_getrandmax()) / mt_getrandmax();
+        $this->setValueInContext($path, $value, $context);
+
+        return $this->result(
+            ResultInterface::OK,
+            'Created the random value "%s".',
+            [$value]
+        );
+    }
+}
---
+++ tests/Process/NodeCode/Data/RandomValueNodeCodeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nodez\Core\Tests\Process\NodeCode\Data;
+
+use Feral\Core\Process\Context\Context;
+use Feral\Core\Process\NodeCode\Data\RandomValueNodeCode;
+use Feral\Core\Process\Result\ResultInterface;
+use PHPUnit\Framework\TestCase;
+
+class RandomValueNodeCodeTest extends TestCase
+{
+
+    private RandomValueNodeCode $node;
+
+    protected function setUp(): void
+    {
+        $this->node = new RandomValueNodeCode();
+    }
+
+    public function testProcess()
+    {
+        $this->node->addConfiguration([
+            RandomValueNodeCode::CONTEXT_PATH => 'random'
+        ]);
+        $context = (new Context());
+        $result = $this->node->process($context);
+        $this->assertEquals(ResultInterface::OK, $result->getStatus());
+        $this->assertNotEmpty($context->get('random'));
+    }
+
+    public function testGetConfigurationDescriptions()
+    {
+        $configuration = $this->node->getConfigurationDescriptions();
+        $this->assertEquals(1, count($configuration));
+    }
+}
